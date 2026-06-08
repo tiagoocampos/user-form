e1b2297ad11b64e0fd244f197db11e5255004b53
Tiago Campos <droiddzn68@gmail.com>
2026-06-08 00:47:44 -0300

    Corrigindo erros nas rotas: a rota delete e rota put estavam usando ListUserController, fiz a correção para as duas utilizarem o controller correto responsável pela ação

--- backend/src/api/routes/userRoutes.ts
+++ backend/src/api/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from 'express';
-import { CreateUserController, ListUserController } from '../controllers/userController.js';
+import { CreateUserController, ListUserController, UpdateUserController, DeleteUserController } from '../controllers/userController.js';
 import { validateSchema } from '../../middlewares/validateSchema.js';
 import { createUserSchema } from '../../schemas/userSchemas.js';
 
@@ -7,7 +7,7 @@ const router = Router();
 
 router.post('/users', validateSchema(createUserSchema), new CreateUserController().handle);
 router.get('/users', new ListUserController().handle);
-router.delete('/users/:id', new ListUserController().handle);
-router.put('/users/:id', new ListUserController().handle);
+router.delete('/users/:id', new DeleteUserController().handle);
+router.put('/users/:id', new UpdateUserController().handle);
 
 export { router };
